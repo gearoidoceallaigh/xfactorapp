a13a1602d917ae091e1a7a59c4fb9696bc804e3a
Gearoid O'Cealligh <gearoid.k@gmail.com>
2011-07-24 22:31:45 +0100

    and again

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
     i = 1
     while i < 145 do
        mins = i * 5
-       Delayed::Job.enqueue SearchJob.new('me'), 0, 5.minutes.from_now.getutc
+       Delayed::Job.enqueue SearchJob.new('me'), 0, mins.minutes.from_now.getutc
        i += 1
     end
     
--- lib/tasks/score.rake
+++ lib/tasks/score.rake
@@ -7,7 +7,7 @@ task :cron => :environment do
   i = 1
   while i < 289 do
      mins = i * 5
-     Delayed::Job.enqueue SearchJob.new('me'), 0, 5.minutes.from_now.getutc
+     Delayed::Job.enqueue SearchJob.new('me'), 0, mins.minutes.from_now.getutc
      i += 1
   end
 end
