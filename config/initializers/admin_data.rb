6ff979de9670a3cfbf6730763d16e893f5dd4548
Gearoid O'Cealligh <gearoid.k@gmail.com>
2011-07-24 20:58:49 +0100

    trying again

--- config/initializers/admin_data.rb
+++ config/initializers/admin_data.rb
@@ -1,4 +1,4 @@
 AdminData.config do |config|
-  config.is_allowed_to_view = lambda {|controller| controller.send('let_me_pass?') }
-  config.is_allowed_to_update = lambda {|controller| controller.send('let_me_pass?') }
+  config.is_allowed_to_view = lambda { return true }
+  config.is_allowed_to_update = lambda { return true }
 end
