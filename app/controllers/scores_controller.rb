6f26980b605da8261f794e4fe545caaff39507ab
Gearoid O'Cealligh <gearoid.k@gmail.com>
2011-12-13 11:29:39 +0000

    updated so that twitter is only polled for remaining candidates

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,6 +1,8 @@
 class ScoresController < ApplicationController
 
   include ScoresHelper
+  include ContestsHelper
+  include ContestantsHelper
 
   # GET /scores
   # GET /scores.xml
@@ -85,42 +87,62 @@ class ScoresController < ApplicationController
   end
   
   def generate_scores
-    require 'time'
-    @client = TwitterSearch::Client.new('app4xfactor')
-    id = params[:id]
+    
+    contest = is_contest_running
+    
+    unless contest.nil?
+    
+      require 'time'
+      @client = TwitterSearch::Client.new('app4xfactor')
+      
+      contestant = Contestant.find(params[:id])  
+    
+      unless contestant.nil? or contestant.eliminated?
 
-    contestant = Contestant.find(id)
+        Rails.logger.info("Getting tweets for: " + contestant.name)
+        
+        unless contestant.search_terms.nil?
+      
+          contestant.search_terms.each do |s| 
 
-    unless contestant.nil? 
-      logger.info("Getting tweets for: " + contestant.name)
-      contestant.search_terms.each do |s| 
-        logger.info("Searching twitter for term: "  + s.value)
-        tweets = @client.query :q => s.value, :rpp => '50'
-        five_mins_ago = Time.now.to_i - 6000
-        count = 0
-        new_tweets = []
-        tweets.each do |tweet|
-          tweet_time = Time.parse(tweet.created_at).to_i          
-          if (tweet_time > five_mins_ago)
-            if check_for_bad_words(tweet)
-              create_bad_score(contestant)
-            else
-              create_good_score(contestant)
+            Rails.logger.info("Searching twitter for term: "  + s.value)
+
+            tweets = @client.query :q => s.value, :rpp => '50'
+            five_mins_ago = Time.now.to_i - 6000
+            count = 0
+            new_tweets = []
+            tweets.each do |tweet|
+              tweet_time = Time.parse(tweet.created_at).to_i    
+                
+              if (tweet_time > five_mins_ago)
+                if check_for_bad_words(tweet)
+                  create_bad_score(contestant, contest)
+                else
+                  create_good_score(contestant, contest)
+                end
+              
+                update_latest_score_for_contest(contestant, contest)
+            
+                new_tweets.push(tweet)
+            
+              end
             end
-            new_tweets.push(tweet)
+            Rails.logger.info("number of new tweets:" + new_tweets.size.to_s)
           end
+      
+          Rails.logger.info('finished polling twitter for ' + contestant.name);
         end
-        logger.info("number of new tweets:" + new_tweets.size.to_s)
-        puts new_tweets.size
       end
       
+      if contestant.nil?
+        Rails.logger.info("Couldn't locate contesant")
+      elsif contestant.eliminated?
+        Rails.logger.info("Contestant with id" + contestant.id + " is eliminated, skipping")
+      end
       
-      puts 'finished'
+    else
+      reset_score_for_contest(contestant)
     end
-    
-    if contestant.nil?
-      logger.debug("Couldn't locate contesant")
-    end
-    
   end
+  
 end
